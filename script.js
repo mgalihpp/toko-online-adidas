f69ec4a5dead908dd2ea840c2952ecdc1857de48
Muhammad Galih <68680851+MGPP1@users.noreply.github.com>
2022-09-14 23:00:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const produk = [...document.querySelectorAll('.produk')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active')
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active')
+    })
+}
+
+produk.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
